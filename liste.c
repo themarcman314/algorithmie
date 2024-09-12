5106af6cb9cd2e39000254708f367089d6ce5c49
Marc Robison <marc.robison@etu.umontpellier.fr>
2024-09-12 11:29:51 +0200

    first commit

---
+++ liste.c
@@ -0,0 +1,15 @@
+struct Node
+{
+	int value;
+	struct Node *next;
+};
+void print_node(struct Node *current)
+{
+	struct Node *temp = current;
+	if(current)
+	{
+
+	}
+
+	return 0;
+}
---
+++ test.c
@@ -0,0 +1,26 @@
+struct Node
+{
+	int value;
+	struct Node *next;
+};
+struct List
+{
+	int n;
+	struct Node *root;
+};
+void add_value(struct List, int value);
+
+
+int main(void)
+{
+	return 0;
+}
+
+
+
+
+
+void add_value(struct List list, int value)
+{
+
+}
